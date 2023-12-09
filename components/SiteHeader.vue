76a9f7e7ffad66b10deea4910e7a38013223c647
Brandon Winger-Air <brandonwinger-air@outlook.com>
2023-12-08 21:49:20 -0500

    index page structure and site header content & styles

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -13,11 +13,12 @@
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+            <!-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" /> -->
+            🖱️
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
-              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white uppercase' : 'text-gray-300 hover:bg-gray-700 hover:text-white uppercase', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
             </div>
           </div>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,32 @@
 <template>
   <section>
     <h1 class="text-5xl font-bold mt-20">
-      👨‍💻 Portfolio + Blog
+      👨‍💻 Project Portfolio
     </h1>
     <p class="text-base text-gray-900 p-2 italic">
       Built with Nuxt 3, Tailwind and GraphQL
     </p>
   </section>
 
-  <section>
-    <h2 class="text-3xl font-bold mt-8">
-      Developed by Brandon Winger-Air
-    </h2>
-    <p class="text-lg py-2">
-      I am a Developer & Designer living in Ontario, Canada with a degree in Enterprise Web & Mobile App Development. 
-      I am a passionate programmer who believes in learning best practices and solving problems, 
-      always striving to improve.
-    </p>
-    <p class="text-lg py-2">
-      I have been developing projects for over 8+ years now and I use my knowledge, passion, and experience to help clients achieve their goals. 
-      I am currently seeking any long-term opportunities and open to contracts or freelance projects. 
-    </p>
-    <p class="text-lg py-2">
-      In my personal life I take care of two cats. 
-      I enjoy playing instruments, video games, reading, running and weightlifting to get me through the week.
-    </p>
+  <section class="flex flex-col md:flex-row">
+    <div class="md:w-3/4">
+      <h2 class="text-3xl font-bold mt-8">
+        Developed by Brandon Winger-Air
+      </h2>
+      <p class="text-lg py-2">
+        I am a Developer and Designer living in Ontario, Canada with a degree in Enterprise Web & Mobile App Development. 
+        I am a passionate programmer who believes in learning best practices and solving problems, 
+        always striving to improve.
+      </p>
+      <p class="text-lg py-2">
+        I have been developing projects for over 8+ years now and use my knowledge, passion, and experience to help freelance clients achieve their goals. 
+        I am currently seeking any long-term opportunities and open to short-term contracts or projects.
+      </p>
+      <p class="text-lg py-2">
+        In my personal life I have two cats and in interest in music, graphics, art and technology. 
+        I enjoy playing instruments, video games, reading, working out and watching shows or documentaries to get me through the week.
+      </p>
+    </div>
+    <img src="~/assets/images/avatar.png" alt="" class="w-1/2 md:max-w-md p-8 mx-auto">
   </section>
 </template>
