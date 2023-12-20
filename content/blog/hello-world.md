329b2d99370f8039a57ca4608b6caa884b3e7e78
Brandon Winger-Air <brandonwinger-air@outlook.com>
2023-12-19 23:24:04 -0500

    add tags & change blog to render Markdown and revise about description

--- content/blog/hello-world.md
+++ content/blog/hello-world.md
@@ -1,8 +1,8 @@
 ---
 title: Hello, World!
 description: This is my very first blog post and I'm so excited to share it with you!
-date: 2023-05-23
-cover: nasa-Q1p7bh3SHj8-unsplash.jpg
+date: 2023-12-19
+cover: https://picsum.photos/300/200
 tags:
   - blog
 ---
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -11,9 +11,29 @@
         Under Construction 🚧
       </p>
       <hr class="w-48 mb-4">
-      <ContentDoc class="prose prose-invert"/>
+      <ContentRenderer :value="data" class="prose prose-invert"/>
+      <div class="my-8">
+        <a 
+          v-for="tag in data.tags" 
+          :key="tag" href="/blog/tags/${tag}" 
+          class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4"
+        >
+          <Icon name="uil:tag" size="1.5rem" class="text-gray-100 mr-2"/>
+          {{ tag }}
+        </a>
+      </div>
     </main>
   </section>
 
   <SiteBottom/>
 </template>
+
+<script setup>
+const { path } = useRoute();
+
+const { data } = await useAsyncData(`content-${path}`, () => {
+  return queryContent()
+    .where({ _path: path })
+    .findOne();
+});
+</script>
