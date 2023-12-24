833855dc9da2da230502699af12da9140c570244
Brandon Winger-Air <brandonwinger-air@outlook.com>
2023-12-24 17:36:00 -0500

    create article list page and blog posts component

---
+++ components/BlogPosts.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="transparent-bg mt-8 p-8">
+    <h1 class="text-3xl mb-8">Blog Posts</h1>
+    <hr class="w-36">
+    <ul>
+      <li v-for="post in posts" :key="post._id" class="my-8 pb-4 border-b-2">
+        <nuxt-link :to="post._path" class="text-2xl">{{ post.title }}</nuxt-link>
+        <p>{{ post.description }}</p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps(['posts'])
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-3xl md:text-5xl mt-10 mt-14 mb-6">
+    🖱️ Website Dev /
+    <span class="font-semibold">Blog</span>
+  </h1>
+  <SiteTagline/>
+
+  <BlogPosts :posts="posts"/>
+</template>
+
+<script setup>
+const { data: posts } = await useAsyncData('posts', () => 
+  queryContent('/blog').find()
+);
+</script>
